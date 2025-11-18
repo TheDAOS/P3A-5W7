b410f4265b6bdce752fc50a7051ad1e6775bffab
TheDAOS <sanjusanthosh46@gmail.com>
2025-11-19 00:57:47 +0530

    Refactor App component structure and enhance index.css for scroll behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div className="bg-orange-400 h-screen overflow-hidden">
-      
+      <h1 className="text-2xl font-extrabold">2048</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
