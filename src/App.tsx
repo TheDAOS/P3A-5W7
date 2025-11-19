65ca33e50dddf897b175f68b0ff003e4c2291721
TheDAOS <sanjusanthosh46@gmail.com>
2025-11-19 23:10:37 +0530

    rm all

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="bg-orange-400 h-screen overflow-hidden">
-      <h1 className="text-2xl font-extrabold">2048</h1>
-    </div>
-  );
-}
-
-export default App;
